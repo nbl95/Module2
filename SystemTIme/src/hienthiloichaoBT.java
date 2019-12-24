3046ef9e0fcd8bf818a39300296a5c589f1df573
nbl95 <nbl.ltu12@gmail.com>
2019-12-24 16:06:02 +0700

    BT hien thi loi chao

---
+++ SystemTIme/src/hienthiloichaoBT.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class hienthiloichaoBT {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter ur Name");
+        String name = scanner.nextLine();
+
+        System.out.println("helo" + name);
+    }
+}
