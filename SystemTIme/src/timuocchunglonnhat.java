6faee2d2a4dd902f910f7840543060760ba32eb4
nbl95 <nbl.ltu12@gmail.com>
2019-12-25 15:31:09 +0700

    tim uoc chung lon nhat

---
+++ SystemTIme/src/timuocchunglonnhat.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class timuocchunglonnhat {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("enter a");
+        a = nhap.nextInt();
+        System.out.println("enter b");
+        b = nhap.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a==0 || b ==0)
+            System.out.println("No greatest common factor");
+
+        while (a !=b){
+            if (a>b)
+                a = a-b;
+            else
+                b=b-a;
+        }
+        System.out.println("Greatest common factor: " + a);
+
+    }
+}
